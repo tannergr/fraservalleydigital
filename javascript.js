aee1997404a469654870d671e53d3996cc9c6701
tannergr <tanner@tlgraham.com>
2018-06-29 22:10:09 -0700

    move images to folder, more mobile friendly stuff

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-var menu = document.querySelector('.')
+console.log("loaded js");
+var showingMenu = false;
+
+function slideMenu(){
+    if(!showingMenu)
+        document.getElementById("sidebar").style.right = 0;
+    else   
+        document.getElementById("sidebar").style.right = -300;
+    showingMenu = !showingMenu;
+}
